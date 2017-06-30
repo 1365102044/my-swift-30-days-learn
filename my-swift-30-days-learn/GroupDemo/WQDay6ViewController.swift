a6ff9c48fe661005a553e34acf5ac31306f564f0
小司机 <1365102044@163.com>
2017-06-30 18:49:05 +0800

    scrollview

--- my-swift-30-days-learn/GroupDemo/WQDay6ViewController.swift
+++ my-swift-30-days-learn/GroupDemo/WQDay6ViewController.swift
@@ -7,28 +7,50 @@
 //
 
 import UIKit
+import SnapKit
 
 @objc (WQDay6ViewController)
-class WQDay6ViewController: WQTableViewRootViewController {
+class WQDay6ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.title = "ScrollView"
+        self.view.backgroundColor = UIColor.white
         
-        self.tableview.tableHeaderView?.addSubview(scrollerview)
-        
+       
+        self.view.addSubview(scrollerview)
+        for  index in 0..<sourData.count {
+            
+            let image = UIImage.init(named: sourData[index] as! String)
+            let imageview = UIImageView.init(image: image)
+            imageview.backgroundColor = UIColor.gray
+            scrollerview.addSubview(imageview)
+            let W = self.view.frame.size.width
+            imageview.snp.makeConstraints({ (make) in
+                make.top.equalTo(scrollerview.snp.top)
+                make.left.equalTo(scrollerview.snp.left).offset(index * 375)
+                make.width.equalTo(W)
+                make.height.equalTo(scrollerview.snp.height)
+            })
+            
+            
+        }
+       
     }
 
     lazy var scrollerview: UIScrollView = {
-        let scrollerview = UIScrollView.init(frame: CGRect(x:0,y:0,width:self.view.frame.width,height:300))
+        let scrollerview = UIScrollView.init(frame: CGRect(x:0,y:0,width:self.view.frame.size.width,height:400))
         scrollerview.bounces = true
+//        scrollerview.isPagingEnabled = true
         scrollerview.alwaysBounceVertical = true
+        scrollerview.contentSize = CGSize(width:self.view.frame.size.width, height:400)
+        scrollerview.center = self.view.center
         return scrollerview
     }()
     
     lazy var sourData: NSArray = {
-        let sourData = NSArray()
-        
+        var sourData = NSArray()
+        sourData = ["100","100","100"]
         return sourData
     }()
     
